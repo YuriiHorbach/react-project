7095e1ef975e5d90d61bf777f0a37c090529ebaf
YuriiHorbach <gyrii84@gmail.com>
2020-11-04 21:41:12 +0200

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className="content">
+    <div className="back-img">
+      <img src="https://eskipaper.com/images/desktop-wallpaper-6.jpg" alt="" />
+    </div>
+    <div className="">
+      ava + description
+    </div>
+    <div className="">
+      My posts
+      <div className="">
+        New post
+      </div>
+      <div className="">
+        post1
+      </div>
+      <div className="">
+        post2
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
