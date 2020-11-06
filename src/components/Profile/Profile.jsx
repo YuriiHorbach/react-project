c9452d9955d50e2fdbb422d051c240310de61bdc
YuriiHorbach <gyrii84@gmail.com>
2020-11-06 23:13:10 +0200

    split components on smaller components

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import s from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts';
 
 
 const Profile = () => {
   return <div className={s.content}>
     <div className={s.backImg}>
+      <img  src="https://eskipaper.com/images/desktop-wallpaper-6.jpg" alt=""/>
     </div>
     <div className="">
       ava + description
     </div>
-    <div className="">
-      My posts
-      <div className={s.item}>
-        New post
-      </div>
-      <div className={s.item}>
-        post1
-      </div>
-      <div className={s.item}>
-        post2
-      </div>
+    <div >
+      <MyPosts />
     </div>
   </div>
 }
