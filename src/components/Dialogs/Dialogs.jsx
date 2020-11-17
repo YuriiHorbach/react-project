a7511ea7b52a6ccdabe582bce681456160ad6b4f
southl <gyrii84@gmail.com>
2020-11-17 15:04:50 +0200

    add users and messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
-    return(
-        <div class ={s.content}>
-            Dialogs
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.dialogActive}>user01</div>
+                <div className={s.dialog}>user02</div>
+                <div className={s.dialog}>user03</div>
+                <div className={s.dialog}>user04</div>
+                <div className={s.dialog}>user05</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>By</div>
+            </div>
         </div>
     )
 }
