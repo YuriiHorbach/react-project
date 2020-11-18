54b1b0452c832815671d0da70e2ef15d08b26361
southl <gyrii84@gmail.com>
2020-11-18 10:42:46 +0200

    Add NavLink to user dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.dialogActive}>user01</div>
-                <div className={s.dialog}>user02</div>
-                <div className={s.dialog}>user03</div>
-                <div className={s.dialog}>user04</div>
-                <div className={s.dialog}>user05</div>
+                <div className={s.dialog + ' ' + s.dialogActive}>
+                    <NavLink to="dialog/1">user01</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="dialog/2">user02</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="dialog/3">user03</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="dialog/4">user04</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="dialog/5">user05</NavLink>
+                </div>
+
             </div>
             <div className={s.messages}>
                 <div className={s.message}>Hi</div>
