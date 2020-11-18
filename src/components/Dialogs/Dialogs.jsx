386d778fad665a66e8f6da223685f1b8cd6ad03f
southl <gyrii84@gmail.com>
2020-11-18 11:01:16 +0200

    Code refactoring: add two components DialogItem and Message

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,32 +2,35 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
+const DialogItem = (props) => {
+    let path ="/dialogs/" + props.id;
+    return(
+        <div className={s.dialog + ' ' + s.dialogActive}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    );
+}
+
+const Message = (props) => {
+    return(
+        <div className={s.message}>{props.message}</div>
+    );
+}
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.dialogActive}>
-                    <NavLink to="dialog/1">user01</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="dialog/2">user02</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="dialog/3">user03</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="dialog/4">user04</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="dialog/5">user05</NavLink>
-                </div>
-
+                <DialogItem name='user01' id='1' />
+                <DialogItem name='user02' id='2' />
+                <DialogItem name='user03' id='3' />
+                <DialogItem name='user04' id='4' />
+                <DialogItem name='user05' id='5' />
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Hi</div>
-                <div className={s.message}>Hello</div>
-                <div className={s.message}>By</div>
+                <Message message='Hi'/>
+                <Message message='Hello'/>
+                <Message message='By'/>
             </div>
         </div>
     )
