5b521ff82b5c7cd246bfee0870b09bf4dcf75dcf
southl <gyrii84@gmail.com>
2020-11-18 14:58:24 +0200

    Refactored Profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 
 const Profile = () => {
-  return <div className={s.content}>
-    <div className={s.backImg}>
-      <img  src="https://eskipaper.com/images/desktop-wallpaper-6.jpg" alt=""/>
+    return <div className={s.content}>
+        <ProfileInfo />
+        <div>
+            ava + description
+        </div>
+        <div>
+            <MyPosts/>
+        </div>
     </div>
-    <div className="">
-      ava + description
-    </div>
-    <div >
-      <MyPosts />
-    </div>
-  </div>
 }
 
 export default Profile;
---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './ProfileInfo.module.css';
+
+const ProfileInfo = (props) => {
+    return <div>
+        <div>
+            {/*<img src="https://eskipaper.com/images/desktop-wallpaper-6.jpg" alt=""/>*/}
+            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" alt=""/>
+        </div>
+    </div>
+}
+
+export default ProfileInfo;
