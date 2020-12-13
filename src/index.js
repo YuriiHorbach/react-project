a1913ee8c91125593c93403b1717ef6ae718bb6a
gyrii84@gmail.com <gyrii84@gmail.com>
2020-12-13 22:15:22 +0200

    remove render.js and use pattern observer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import App from './App';
-import state from './redux/state.js';
-import {addPost} from "./redux/state.js";
-import {rerenderEntireTree} from "./render";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {BrowserRouter} from "react-router-dom";
+import state, {addPost, subscribe, updateNewPostText} from "./redux/state.js";
+
+let rerenderEntireTree = (state)=> {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state = {state} addPost = {addPost} updateNewPostText = {updateNewPostText}/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree)
